ae1e7e434930ede3c7750c0bd3ea984f30cbadb9
Marcelo Dias <marc29dias@yahoo.com.br>
2020-01-27 11:05:56 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { Component, Injectable } from '@angular/core';
 
 @Component({
   selector: '#app-root',
-  styleUrls : ['app.component.scss'],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
